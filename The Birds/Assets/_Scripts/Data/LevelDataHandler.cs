39dbc87bda1daa1172fc143c558cd0ca9abb9db4
FunGi2410 <nguyennhanit2410@gmail.com>
2024-06-23 23:29:49 +0700

    save and load level number

--- The Birds/Assets/_Scripts/Data/LevelDataHandler.cs
+++ The Birds/Assets/_Scripts/Data/LevelDataHandler.cs
@@ -12,15 +12,17 @@ public class LevelDataHandler : DataHandler
     private void Awake()
     {
         this.levelManager = GetComponent<LevelManager>();
+
+        Load();
     }
     public override void Load()
     {
-        string path = Application.persistentDataPath + ".player.fun";
+        string path = Application.persistentDataPath + "/level.fun";
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
             FileStream stream = new FileStream(path, FileMode.Open);
-
+            stream.Position = 0;
             LevelData data = formatter.Deserialize(stream) as LevelData;
             this.levelManager.LoadData(data);
             stream.Close();
@@ -34,12 +36,17 @@ public class LevelDataHandler : DataHandler
     public override void Save()
     {
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath + ".player.fun";
+        string path = Application.persistentDataPath + "/level.fun";
         FileStream stream = new FileStream(path, FileMode.Create);
 
         LevelData data = new LevelData(levelManager);
 
         formatter.Serialize(stream, data);
         stream.Close();
+    }
+
+    private void OnApplicationQuit()
+    {
+        Save();
     }
 }
--- The Birds/Assets/_Scripts/Managers/LevelManager.cs
+++ The Birds/Assets/_Scripts/Managers/LevelManager.cs
@@ -1,6 +1,8 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[Serializable]
 public class LevelData
 {
     public int curNumberLevel;
